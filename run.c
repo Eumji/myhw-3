3e002873e6330b4b681b525b9e139c77127a1d1d
jikoong <jihye97@ajou.ac.kr>
2017-12-19 22:09:40 +0900

    modify

--- run.c
+++ run.c
@@ -1,23 +1,30 @@
 #include <sys/types.h>
 #include <sys/resource.h>
 #include <limits.h>
+#include <stdio.h>
 
 #include "run.h"
 #include "util.h"
 
-void *base = 0;
+#define NULL (void*)0
 
-// RETURN NULL IF NOT EXIST PROPER SPACE
+void *end = 0;
+void *base = 0;
+void *last = 0;
+
+// RETURN 0 IF NOT EXIST PROPER SPACE
 
 p_meta find_meta(p_meta *last, size_t size) {
 	p_meta index = base;
-	p_meta result = NULL;
+	p_meta result = 0;
+
+	if(last == base) return 0;
 
 	switch(fit_flag){
 		case FIRST_FIT:
 			{
-				while(index != NULL){
-					if(index->free == 0 && index->size >= size){
+				while(index != 0){
+					if(index->free != 0 && index->size >= size){
 						result = index;
 						break;
 					}
@@ -29,34 +36,37 @@ p_meta find_meta(p_meta *last, size_t size) {
 
 		case BEST_FIT:
 			{
-				p_meta best = NULL;
-				
+				p_meta best = 0;	
 
-				while(index != NULL){
-					if((index->size) >=size){
-						if(best == NULL)
-							best = index;
-						else if(best->size > index->size)
-							best = index;
-					}
+				while(index != 0){
+					if(index->free != 0)
+						if((index->size) >=size){
+							if(best == 0)
+								best = index;
+							else if(best->size > index->size)
+								best = index;
+						}
 					index = (index->next);
 				}
+				result = best;
 			}
 			break;
 
 		case WORST_FIT:
 			{
-				p_meta worst = NULL;
+				p_meta worst = 0;
 
-				while(index != NULL){
-					if((index->size) >= size){
-						if(worst == NULL)
-							worst = index;
-						else if(best->size < index->size)
-							worst = index;
-					}
+				while(index != 0){
+					if(index->free !=0)
+						if((index->size) >= size){
+							if(worst == 0)
+								worst = index;
+							else if(worst->size < index->size)
+								worst = index;
+						}
 					index = (index->next);
 				}
+				result = worst;
 			}
 			break;
 
@@ -65,47 +75,62 @@ p_meta find_meta(p_meta *last, size_t size) {
 }
 
 void *m_malloc(size_t size) {
+
 	p_meta find;
+
+	size += (size&3) ? 4-(size&3) : 0;
 
 	find = find_meta(end, size);
 
 	// find proper place
 
-	if(list != NULL){
-		// fill 0 
+	if(find != 0){
+
+		// fill 0
 		memset(find, 0, size + META_SIZE);
-		find->free = 1;
+		find->free = 0;
 
 		// if exist rest 
 		if(find->size != size + META_SIZE){
 			p_meta list = find + size + META_SIZE;
-			set(list, 0, find->next, find, (find->size)-size-META_SIZE);
+			list->free = 1;
+			list->next = find->next;
+			list->prev = find;
+			list->size = (find->size) - size - META_SIZE;
 			list->next = find->next;
 		}
-		
+
 		find->size -= size + META_SIZE;
-		
+
 		//TODO : MERGING IF NEXT THING IS FREE
-		
+
 		return find->data;
 	}
-	else{ // add new place
-		if(base == 0)
-			base = end = sbrk(0);
 
-		//if cannot allocate new things, return NULL
+	// new area
+	else{
+		if(base == 0)
+			base = end = find = last = sbrk(0);
 
 		if(sbrk(end+size+META_SIZE) == -1) 
-			return NULL;
+			return (void*)0;
 
-		end += size + META_SIZE; 
-		
-		// make new one
-		p_meta list;
+		p_meta list = end;
+		p_meta cur = last;
 
-		set(list, 0, end, NULL, size + META_SIZE);
+		if(last != list){
+			cur->next = list;
+			list->prev = last;
+		}
+		else
+			list->prev = 0;
 
-		find->next = list;
+		list->free = 0;
+		list->next = 0;
+		list->size = size + META_SIZE;
+		last = list;
+
+		end += size + META_SIZE;
 
 		return list->data;
 	}
@@ -115,14 +140,6 @@ void m_free(void *ptr) {
 
 }
 
-void* m_realloc(void* ptr, size_t size)
-
-}
-
-void set(p_meta x, int free, p_meta next, p_meta prev, int size){
-	(x->free) = free;
-	(x->next) = next;
-	(x->prev) = prev;
-	(x->size) = size;
+void* m_realloc(void* ptr, size_t size){
 }
 
